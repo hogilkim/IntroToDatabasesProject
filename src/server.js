0f27fc4a550adb4f720ae0943697ac930f22942f
Hogyeong Kim <hogilkim@gmail.com>
2021-04-21 16:42:36 +0900

    test 1

--- src/server.js
+++ src/server.js
@@ -3,6 +3,10 @@ const app = express();
 
 const PORT = process.env.PORT || 8000;
 
+app.get('/', (res, req)=>{
+    res.setEncoding('Hello from express');
+})
+
 app.listen(PORT, ()=> {
     console.log(`listening on ${PORT}`)
 })
