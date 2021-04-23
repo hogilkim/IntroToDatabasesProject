6838586a3bbb5c7081aabe1e1bccf29fe5657d3d
hogilkim <49915234+hogilkim@users.noreply.github.com>
2021-04-23 22:29:10 +0900

    Update Flight.js

--- src/models/Flight.js
+++ src/models/Flight.js
@@ -2,15 +2,27 @@ const mongoose = require('mongoose');
 
 const FlightSchema = new mongoose.Schema({
     flight_number: String,
-    airline_name: String,
-    depart_airport: String,
+    airline_name: {
+        type: String,
+        ref: "Airline"
+    },
+    depart_airport: {
+        type: String,
+        ref: "Airport"
+    },
     depart_time: String,
     depart_date: Date,
-    arrival_airport: String,
+    arrival_airport: {
+        type: String,
+        ref: "Airport"
+    },
     arrival_time: String,
     arrival_date: Date,
     base_price: Number,
-    airplane_id: String,
+    airplane_id: {
+        type: String,
+        ref: "Airplane"
+    },
     status: String,
 });
 
