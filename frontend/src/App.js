465a255d6126b800364882db64ae1e3f34099701
Hogyeong Kim <hogilkim@gmail.com>
2021-05-05 09:59:20 +0900

    frontend start

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+          Flight Ticket Reservation System
+    </div>
+  );
+}
+
+export default App;
