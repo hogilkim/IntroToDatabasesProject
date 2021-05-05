cd22b70b8230553f2850e8a7dd845cefdf47aa9e
hogilkim <hogilkim@gmail.com>
2021-05-05 13:19:30 +0900

    from desktop comp

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
-
+//test
 export default function Routes(){
     return(
         <BrowserRouter>
